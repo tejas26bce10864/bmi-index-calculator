c340ad93bd78c08a3241e3c4e9799d020b386813
tejas26bce10864 <tejas.26bce10864@vitbhopal.ac.in>
2026-09-22 23:02:13 +0530

    Add files via upload

---
+++ Tejas Vityarti project BMI Index Calcular.py
@@ -0,0 +1,8 @@
+#BMI Index Calculator
+w=int(input("Enter your Weight(in Kg)"))
+print("your entered weight is ",w)
+h=float(input("Enter your Height (in cm)"))
+print("your entered Height is ",h)
+#BMI=Weight(in kg)/Height**2(in cm)
+BMI=w/h**2
+print("BMI INDEX =",BMI)
